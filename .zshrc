f86e502621155b8dcb62b275d82246a0d87bd7ac
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2023-11-10 15:05:37 +0100

    update to current workflow

--- .zshrc
+++ .zshrc
@@ -5,16 +5,10 @@
 export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="frisk" # set by `omz`
 BAT_THEME="Dracula"
+
 # Golang Env
 export PATH="$PATH:$GOPATH/bin"
 export GOPATH="$HOME/go"
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
-# You can set one of the optional three formats:
-# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# or set a custom format using the strftime function format specifications,
-# see 'man strftime' for details.
-# HIST_STAMPS="mm/dd/yyyy"
 
 plugins=(
   git
@@ -28,40 +22,18 @@ plugins=(
 source $ZSH/oh-my-zsh.sh
 
 # User configuration
-
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
+export MANPATH="/usr/local/man:$MANPATH"
+export LANG=en_US.UTF-8
 
 # Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='mvim'
+fi
 
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
-
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
-#
-export GITHUB_ACCESS_TOKEN="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"
-alias genssh="ssh-keygen -t rsa && pbcopy < ~/.ssh/id_rsa.pub"
 alias k="kubectl"
-alias kn="kubectl -n rc-system"
-alias wkn="watch kubectl -n rc-system"
-alias kpn"kubectl get pods -n rc-system"
-alias kdpn"kubectl describe pod -n rc-system"
 alias watch="watch -n .5"
-#alias history="history -f"
 alias g="git"
 alias ga="git add"
 alias gc="git commit -m"
@@ -70,22 +42,11 @@ alias gpuf="git push -f"
 alias gpu="git pull"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias gri="git rebase -i"
-alias dev="cd ~/dev/git/reynencourt"
 alias mk="minikube"
 alias python="python3"
-alias pcalc="python /Users/cksidharthan/dev/git/other/scripts/percentage_calculator.py"
-
 
 # IP
 alias localip="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
-
-# Reynen court Links
-alias cr="open https://certified-registry.rcplatform.io"
-alias cr="open https://staging-registry.rcacc.link/"
-alias awso="https://reynencourt.awsapps.com/start#/"
-alias jira="open https://reynencourt.atlassian.net/jira/software/projects/PRODEV/boards/8"
-alias office="open https://reynencourt.ezofficeinventory.com/users/sign_in"
-alias paperdocs="open https://paper.dropbox.com"
 
 # for terminal colors
 export CLICOLOR=1
@@ -94,36 +55,24 @@ export PATH="/opt/homebrew/opt/node@12/bin:$PATH"
 export GIT_TERMINAL_PROMPT=1
 export GOSUMDB=off
 
-
-# To Display the time taken for previous command to run
-
-ifunction preexec() {
-    timer=${timer:-$SECONDS}
-}
-
-function precmd() {
-    if [ $timer ]; then
-        timer_show=$(($SECONDS - $timer))
-        timer_show=$(printf '%.*f\n' 3 $timer_show)
-        export RPROMPT="${timer_show}s"
-        unset timer
-    fi
-}
-
 # go Project commands
 alias golint="go vet ./..."
 alias gotest="go test --count=1 -coverprofile=coverage.out ./pkg/..."
 
-# others
-alias weather="curl \"wttr.in/Amsterdam?format=3\""
+export HELM_EXPERIMENTAL_OCI=1
 
-# Cloud
-alias bastion="ssh -i ~/.ssh/keys/kraken-new.pem ubuntu@18.184.139.13"
+alias keyRepeatOn="defaults write -g ApplePressAndHoldEnabled -bool false"
+alias keyRepeatOff="defaults write -g ApplePressAndHoldEnabled -bool true"
 
+# Kubernetes Autocompletions
+autoload -Uz compinit
+compinit
+source <(kubectl completion zsh)
 
-# FZF Color Schemes
-export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
---color=dark
---color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f
---color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7
-'
+# Go
+alias makedeps="go mod download && go mod tidy && go mod verify && go mod vendor"
+alias findbyport="sudo lsof -i -P | grep LISTEN | grep :$PORT"
+
+# Startship Config
+eval "$(starship init zsh)"
+export STARSHIP_CONFIG=~/.config/starship/starship.toml
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Ask for the administrator password upfront.
 sudo -v
@@ -54,6 +54,12 @@ defaults write com.apple.dock autohide-delay -float 0
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
 
+# Move files
+cp .zshrc ~/.zshrc
+cp .gitconfig ~/.gitconfig
+mkdir -p ~/.config/starship
+cp starship.toml ~/.config/starship/starship.toml
+
 # Check for Homebrew, and then install it
 if test ! "$(which brew)"; then
     echo "Installing homebrew..."
@@ -63,47 +69,23 @@ else
     echo "Homebrew already installed!"
 fi
 
-# Install XCode Command Line Tools
-echo 'Checking to see if XCode Command Line Tools are installed...'
-brew config
-
-# Updating Homebrew.
-echo "Updating Homebrew..."
+# Setup Homebrew
+echo "Setting up Homebrew..."
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/sid/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+brew analytics off
 brew update
-
-# Upgrade any already-installed formulae.
-echo "Upgrading Homebrew..."
 brew upgrade
-
-# Install iTerm2
-echo "Installing iTerm2..."
-brew cask install iterm2
 
 # Update the Terminal
 # Install oh-my-zsh
 echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo "Need to logout now to start the new SHELL..."
-logout
 
 # Install Git
 echo "Installing Git..."
 brew install git
-
-# Install Powerline fonts
-echo "Installing Powerline fonts..."
-git clone https://github.com/powerline/fonts.git
-cd fonts || exit
-sh -c ./install.sh
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install nmap
-
-# Install other useful binaries.
-brew install speedtest_cli
-
-# Core casks
-brew install --cask --appdir="/Applications" alfred
 
 # Development tool casks
 brew install --cask --appdir="/Applications" visual-studio-code
@@ -114,60 +96,79 @@ brew install --cask --appdir="/Applications" slack
 brew install --cask --appdir="/Applications" goland
 brew install --cask --appdir="/Applications" github
 brew install --cask --appdir="/Applications" iterm2
-brew install --cask --appdir="/Applications" zoomus
-brew install --cask --appdir="/Applications" telegram-desktop
 brew install --cask --appdir="/Applications" docker
-brew install --cask --appdir="/Applications" docker-toolbox
 brew install --cask --appdir="/Applications" brave-browser
-brew install --cask --appdir="/Applications" caffeine
+brew install --cask --appdir="/Applications" arc
+brew install --cask --appdir="/Applications" postman
+brew install --cask --appdir="/Applications" google-chrome
+brew install --cask --appdir="/Applications" appcleaner
 
-brew install docker-compose
-brew install minikube
-brew install podman
-brew install calendar
-brew install jq
-brew install node@12
-brew install wget 
-brew install ytt
-brew install golangci-lint
-brew install act
-brew install neofetch
-brew install yamllint
-brew install hadolint
-brew install watch
-brew install vue-cli
-brew install go
-brew install zsh-syntax-highlighting
-brew install zsh-autosuggestions
-brew install zsh-you-should-use
+brew install docker-compose \
+    minikube \
+    jq \
+    node \
+    npm \
+    wget \
+    neovim \
+    neofetch \
+    yamllint \
+    hadolint \
+    watch \
+    go \
+    zsh-syntax-highlighting \
+    zsh-autosuggestions \
+    zsh-you-should-use \
+    starship \
+    zsh-autosuggestions \
+    zsh-syntax-highlighting \
+    azure-cli \
+    k6 \
+    fzf
+
+# Install bun
+brew tap oven-sh/bun # for macOS and Linux
+brew install bun
 
 # Remove outdated versions from the cellar.
 echo "Running brew cleanup..."
 brew cleanup
 
+## Move fonts from fonts folder to system fonts
+echo "Moving fonts to system fonts..."
+cp -R fonts/* ~/Library/Fonts/
+
 # Change default shell to zsh
 echo "Changing default shell to zsh..."
 chsh -s $(which zsh)
 
-# zsh settings
-cp .frisk.zsh-theme ~/.oh-my-zsh/themes/
-cp .zshrc ~/.zshrc
-mkdir -p ~/.vim/plugconf
-cp coc-conf.vim ~/.vim/plugconf/
-cp .vimrc ~/.vimrc
-cp .gitconfig ~/.gitconfig
+# Install NVChad
+echo "Installing NVChad..."
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
 
-echo "-----------------"
-echo "-- Still to do --"
-echo "-----------------"
-echo "1. Set Jebrains Mono as the default font in iTerm2, Goland, and Visual Studio Code"
-echo "2. Set the default shell to zsh"
-echo "3. Install eclipse keymap for Goland"
+# Install Golang Command Line Tools
+go install golang.org/x/tools/cmd/goimports@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+go install github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@latest
+go install go.uber.org/mock/mockgen@latest
+go install github.com/nikolaydubina/go-cover-treemap@latest
+go install github.com/securego/gosec/v2/cmd/gosec@latest
+
+# Install NPM Packages
+npm install -g @redocly/cli@latest
+npm install -g @quobix/vacuum
+npm install -g newman jsonlint newman-reporter-htmlextra
+
+# FZF
+$(brew --prefix)/opt/fzf/install --all
+
+# Install ZSH Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 source ~/.zshrc
-
-vim -E -s -u "$HOME/.vimrc" +PlugInstall +qall
-vim -R -s -u +GoInstallBinaries +qall
-vim -E -s -u +CocInstall coc-yaml coc-json coc-tsserver coc-go 
-
+echo "\n\n\n"
 echo "You're done!"
+echo "------------------------------"
+echo "You still need to install Amphetamine, Magnet and other apps manually from the App Store."
+echo "Please restart your computer to complete the installation"
+echo "------------------------------"
