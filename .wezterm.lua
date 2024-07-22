4e3c8a384097ee761fe20e3a9f22e2d4a328b8de
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2024-07-22 22:41:05 +0200

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,10 +4,12 @@ local config = wezterm.config_builder()
 config.color_scheme = "Dark+"
 config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 1
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 90
 config.bold_brightens_ansi_colors = true
-config.font_size = 14.5
-config.line_height = 1.2
+config.font_size = 15
+config.line_height = 1.1
+config.window_decorations = "RESIZE"
 
 local function is_vim(pane)
 	local process_info = pane:get_foreground_process_info()
